5861d00702ed1625bb9867cfebf17933e402021b
DanielMarque <daniel.cmarques@stone.com.br>
2022-02-07 17:32:18 -0300

    feat: passing one test

--- users/tests/routes/index.test.ts
+++ users/tests/routes/index.test.ts
@@ -2,10 +2,25 @@ import { app } from '../../index'
 import request from 'supertest'
 
 
-describe('Should test all routes', () => {
-
-  it('', async () => {
-    const response = await request(app).get('/users')
-    await expect(response).resolves.toBe({})
+describe('Test all routes', () => {
+// Criar filas para tratativa de teste
+  it('Should get all users in db', async () => {
+    const response = await request(app._router).get('/users')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    expect(JSON.parse(response.text)).toEqual({
+      result: [
+        {
+          birthdate: '04/09/1997',
+          id: '3812f5dd-2432-4eaa-96f4-51b050d7bee0',
+          name: 'Daniel Marques ',
+        },
+        {
+          birthdate: '04/09/1997',
+          id: 'b5ade2c0-8152-11ec-a8a3-0242ac120002',
+          name: 'Daniel Marques',
+        },
+      ],
+    })
   })
 })
